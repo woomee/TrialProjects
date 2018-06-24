f3767c31655730a7ff51ed373c0edd09e7d6d993
e.umino <e.umino@gmail.com>
2018-06-25 01:24:03 +0900

    パラメータでテーブル名やDBをまとめて指定してループして実行するようにした
    最新はConvertCSVLoop05.ktrとjava

---
+++ TrialPentahoDI/src/test/java/ConvertCSVLoop04_2.java
@@ -0,0 +1,240 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.TransformClassBase;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClass;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClassData;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClassMeta;
+
+/**
+ * Pentaho DIにてCSV変換のTransformをループして繰り返し処理ができるか試す
+ *
+ * processRow()の中で一つのテーブルに対する処理を行うようにした。
+ * 　⇒ただ、1つの大きいファイルではDBに入れるのが後になってしまうのが問題
+ * 　　⇒変換途中でprocessRowを抜ける処理を入れる  (ConverterCSVLoop03)
+ *
+ * @author Eiichiro Umino
+ *
+ */
+public class ConvertCSVLoop04_2 extends TransformClassBase {
+
+	public ConvertCSVLoop04_2(UserDefinedJavaClass parent, UserDefinedJavaClassMeta meta, UserDefinedJavaClassData data)
+			throws KettleStepException {
+		super(parent, meta, data);
+	}
+
+	///////////////////////////////////////////////////
+	// ここからユーザ定義Javaステップへコピー
+	// import文もpentaho以外のものはコピーする。
+	///////////////////////////////////////////////////
+
+	/* ktr側で設定されているパラメータ */
+	static private String PARAM_IN_FILE = "IN_FILE";
+	static private String PARAM_START_ROW = "START_ROW";
+
+	/* 前のステップから渡されたフィールド */
+	static private String FIELD_DB_TABLE = "db_table";
+	static private String FIELD_DB_USER = "db_user";
+	static private String FIELD_DB_PASS = "db_pass";
+
+
+	/**
+	 * 変換を実行する内部クラス
+	 */
+	private class Extractor {
+
+		private BufferedReader _reader = null;
+
+		/**
+		 * ファイルを読み込み初期化
+		 * @throws KettleException
+		 */
+		public void init() throws KettleException {
+			try {
+				/* パラメータで指定されたファイルを読み込む */
+				String filePathStr = getVariable(PARAM_IN_FILE);
+				logBasic("In FilePath=" + filePathStr);
+				Path filePath = Paths.get(filePathStr, new String[0]);
+				_reader = Files.newBufferedReader(filePath);
+
+				/* パラメータで指定された最初の行まで読み込む */
+				int startRow = Integer.parseInt(getVariable(PARAM_START_ROW));
+				for (int i = 0; i < startRow; i++) {
+					_reader.readLine();
+				}
+			} catch (IOException e) {
+				throwAsKettleException(e);
+			}
+		}
+
+		/**
+		 * インポート対象のカラム名情報を返却する。
+		 *
+		 * @return
+		 */
+		public ValueMetaInterface[] getRowColumns() {
+			return new ValueMetaInterface[] {
+					new ValueMetaString("col1"),
+					new ValueMetaString("col2")
+			};
+		}
+
+		/**
+		 * ファイルを1行読み込んで配列を返す.
+		 *
+		 * @return 読み込んだデータ。終端の場合はnul値とする。
+		 */
+		public Object[] getRowData() throws KettleException {
+			if (_reader == null) {
+				return null;
+			}
+			String line = null;
+			try {
+				line = _reader.readLine();
+			} catch (IOException e) {
+				throwAsKettleException(e);
+			}
+			if (line == null) {
+				return null;
+			}
+
+			String[] columnDatas = line.split(",");
+
+			/*
+			 * 必要に応じて変換処理
+			 */
+			// 大文字にする
+			columnDatas[1] = toUpper(columnDatas[1]);
+
+			return columnDatas;
+		}
+
+		public String toUpper(String indata) {
+			return indata.toUpperCase();
+		}
+
+		public void close() throws KettleException {
+			if (_reader != null) {
+				try {
+					_reader.close();
+				} catch (IOException e) {
+					throwAsKettleException(e);
+				}
+			}
+		}
+
+		private void throwAsKettleException(Throwable e) throws KettleException {
+			logError("エラーが発生", e);
+			throw new KettleException(e);
+		}
+	}
+
+	Extractor extractor = new Extractor();
+
+	String _tableName = "";
+	static int PROCESS_COUNT = 2;
+	private boolean _process_contiue = false;
+
+	/**
+	 * @return 次の行へ継続する場合はtrue, 終了する場合はfalse
+	 */
+	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
+
+		/* 最初の1回目(1行目)はfirst == trueとなる */
+		if (first) {
+			first = false;
+			logBasic("first");
+		}
+
+		/* processの途中ではない場合(_process_contiue == false)の場合は
+		 * テーブル名を前のステップから取得。
+		 * null値になった場合は終了 */
+		if (!_process_contiue) {
+			Object[] getRowFromPrivStep = getRow();
+			if (getRowFromPrivStep == null) {
+				logBasic("inputRow==null");
+				setOutputDone();
+				return false;
+			}
+			_tableName = get(Fields.In, FIELD_DB_TABLE).getString(getRowFromPrivStep);
+			logBasic("db_table=" + _tableName);
+
+			String db_user = get(Fields.In, FIELD_DB_USER).getString(getRowFromPrivStep);
+			setVariable(FIELD_DB_USER, db_user);
+			logBasic("db_user=" + db_user);
+
+			String db_pass = get(Fields.In, FIELD_DB_PASS).getString(getRowFromPrivStep);
+			setVariable(FIELD_DB_PASS, db_pass);
+			logBasic("db_pass=" + db_pass);
+
+
+
+			/* コンバータの初期化 */
+			extractor.init();
+		}
+		_process_contiue = false;
+
+		/*
+		 * テーブルに対する変換を実行
+		 *
+		 * PROCESS_COUNT毎に処理を分割する
+		 */
+		Object[] rowData = null;
+		int count = 0;
+		while ((rowData = extractor.getRowData()) != null) {
+			/* データの先頭にテーブル名を入れる */
+			Object[] rowDataWithTable = new Object[rowData.length + 1];
+			System.arraycopy(new String[] { _tableName }, 0, rowDataWithTable, 0, 1);
+			System.arraycopy(rowData, 0, rowDataWithTable, 1, rowData.length);
+
+			/*
+			 * 出力する列データを設定
+			 * クリアしてから再設定することで途中でカラム構成が変わっても対応する。
+			 * 1列目はテーブル名を入れる
+			 * */
+			RowMetaInterface newFields = new RowMeta();
+			newFields.addValueMeta(new ValueMetaString(FIELD_DB_TABLE));
+			ValueMetaInterface[] columns = extractor.getRowColumns();
+			for (int i = 0; i < columns.length; i++) {
+				newFields.addValueMeta(columns[i]);
+			}
+			data.outputRowMeta.clear();
+			data.outputRowMeta.addRowMeta(newFields);
+
+			/*
+			 * 1行のデータを出力
+			 */
+			putRow(data.outputRowMeta, rowDataWithTable);
+			logBasic("putRow");
+
+			count++;
+			if (count >= PROCESS_COUNT) {
+				_process_contiue = true;
+				break;
+			}
+		}
+
+		/* processが終了した時(_process_contiue==true)のみ
+		 *  コンバータのクローズ */
+		if (!_process_contiue) {
+			extractor.close();
+		}
+
+		return true;
+
+	}
+
+	//// ここまで
+
+}
---
+++ TrialPentahoDI/src/test/java/ConvertCSVLoop05_1.java
@@ -0,0 +1,141 @@
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.core.row.RowDataUtil;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.TransformClassBase;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClass;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClassData;
+import org.pentaho.di.trans.steps.userdefinedjavaclass.UserDefinedJavaClassMeta;
+
+/**
+ * Pentaho DIにてCSV変換のTransformをループして繰り返し処理ができるか試す
+ *
+ * [利用方法]
+ * ・Pentaho DIのlibクラスから以下をクラスパスに追加
+ *   kettele-core-xxxx.jar
+ *   kettel-engine-xxxx.jar
+ *
+ * [参考]
+ * http://type-exit.org/adventures-with-open-source-bi/2010/10/the-user-defined-java-class-step
+ *
+ * [処理内容]
+ * ・環境変数DB_TABLESを取得。カンマ区切りで分割して次の出力とする。
+ * ・全てのDB_TABLESを出力したら処理は終了
+ *
+ * [トライアルポイント]
+ * ・DB_TABLESを次のステップに渡すことができるか？
+ * ・渡されたテーブルに対してTransformを実行できているか？
+ *   ⇒実行できるが、それぞれのステップが独立で動くため、TB1に1レコード、TB2に残りの全レコードが入ってしまう
+ *
+ * @author Eiichiro Umino
+ *
+ */
+public class ConvertCSVLoop05_1 extends TransformClassBase {
+
+	public ConvertCSVLoop05_1(UserDefinedJavaClass parent, UserDefinedJavaClassMeta meta, UserDefinedJavaClassData data)
+			throws KettleStepException {
+		super(parent, meta, data);
+	}
+
+	///////////////////////////////////////////////////
+	// ここからユーザ定義Javaステップへコピー
+	// import文もpentaho以外のものはコピーする。
+	///////////////////////////////////////////////////
+
+	/* ktrのパラメータとして受け取る */
+	static private String PARAM_DB_TABLES = "DB_TABLES";
+	static private String PARAM_DB_USERS = "DB_USERS";
+	static private String PARAM_DB_PASSWORDS = "DB_PASSWORDS";
+	static private String PARAM_START_ROWS = "START_ROWS";
+
+	/* 次のステップへフィールドとして渡す */
+	static private String FIELD_DB_TABLE = "db_table";
+	static private String FIELD_DB_USER = "db_user";
+	static private String FIELD_DB_PASS = "db_pass";
+	static private String FIELD_START_ROW = "start_row";
+
+
+	/* PARAMとFIELDをまとめたもの。個数と順序で対応していることが前提 */
+	static private String[] PARAMS = {
+			PARAM_DB_TABLES,
+			PARAM_DB_USERS,
+			PARAM_DB_PASSWORDS,
+			PARAM_START_ROWS
+	};
+	static private String[] FIELDS = {
+			FIELD_DB_TABLE,
+			FIELD_DB_USER,
+			FIELD_DB_PASS,
+			FIELD_START_ROW
+	};
+
+	private String[][] _allParams = null;
+	private int _currentIdx = 0;
+
+	/**
+	 * @return 次の行へ継続する場合はtrue, 終了する場合はfalse
+	 */
+	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
+
+		/* 最初の1回目(1行目)はfirst == trueとなる */
+		if (first) {
+			first = false;
+			logDebug("first");
+
+			/* パラメータをカンマ区切りして取得 */
+			_allParams = getAllParams(PARAMS);
+		}
+
+		/* カレントのデータを取得してouputDataに設定 */
+		String currentParams[] = getParams(_allParams, _currentIdx);
+		Object[] outputField = RowDataUtil.allocateRowData(FIELDS.length);
+		for (int i = 0; i < FIELDS.length; i++) {
+			logDebug("Set Field: " + FIELDS[i] + "=" + currentParams[i]);
+			get(Fields.Out, FIELDS[i]).setValue(outputField, currentParams[i]);
+		}
+
+		/* 次のステップへフィールドで渡す */
+		putRow(data.outputRowMeta, outputField);
+
+		/* 全てのdb_table(_paramVariables[0])を渡したら終了する */
+		_currentIdx++;
+		if (_currentIdx >= _allParams[0].length) {
+			logDebug("Finish");
+			setOutputDone();
+			return false;
+		}
+
+		return true;
+	}
+
+	private String[][] getAllParams(String[] paramKeys) {
+		String[][] variables = new String[paramKeys.length][];
+		for (int i = 0; i < paramKeys.length; i++) {
+			String[] vals = getVariable(paramKeys[i]).split(",");
+			variables[i] = vals;
+		}
+
+		for (int i = 0; i < variables.length; i++) {
+			for (int j = 0; j < variables[i].length; j++) {
+				logDebug("AllVariables[" + i + "][" + j + "]: " + variables[i][j]);
+			}
+		}
+
+
+		return variables;
+	}
+
+	private String[] getParams(String[][] allParams, int index) {
+		String[] variables = new String[allParams.length];
+		for (int i = 0; i < variables.length; i++) {
+			variables[i] = allParams[i][index];
+			logDebug("CurrentVariable[" + i + "]: " + variables[i]);
+		}
+		return variables;
+	}
+
+	////////////////////////////////////////////////////////////
+	/// ここまで
+
+}
