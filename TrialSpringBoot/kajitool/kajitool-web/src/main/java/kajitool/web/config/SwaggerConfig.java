29c51c5492f7d8204cd44329ba0ae834a321667c
E.Umino <e.umino@gmail.com>
2021-11-16 07:40:42 +0900

    kajitoolのswaggerのバージョンを2.9から3.0に対応

--- TrialSpringBoot/kajitool/kajitool-web/src/main/java/kajitool/web/config/SwaggerConfig.java
+++ TrialSpringBoot/kajitool/kajitool-web/src/main/java/kajitool/web/config/SwaggerConfig.java
@@ -1,24 +1,39 @@
 package kajitool.web.config;
 
-import com.google.common.base.Predicate;
+// import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static com.google.common.base.Predicates.containsPattern;
-import static com.google.common.base.Predicates.or;
+// import static com.google.common.base.Predicates.containsPattern;
+// import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    
+    // for swagger 2.9
+    // @Bean
+    // public Docket document() {
+    //     return new Docket(DocumentationType.SWAGGER_2)
+    //             .select().paths(paths()).build();
+    // }
+    // private Predicate<String> paths() {
+    //     return or(containsPattern("/api*"));
+    // }
+
+    // for swagger 3.0
     @Bean
-    public Docket document() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().paths(paths()).build();
-    }
-    private Predicate<String> paths() {
-        return or(containsPattern("/api*"));
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.regex("/api/.*"))                          
+          .build();                                           
     }
 }
